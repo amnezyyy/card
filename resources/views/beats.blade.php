f194eea779ba6db9a20b98850001425277d74f5a
amenzyyy <fomin.casch@yandex.ru>
2023-07-09 17:11:40 +0300

    amnezyyy

--- resources/views/beats.blade.php
+++ resources/views/beats.blade.php
@@ -1,16 +1,15 @@
 <!doctype html>
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>beats</title>
-
     <link rel="stylesheet" type="text/css" href="{{asset('css/beats.css')}}">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://cdn.jsdelivr.net/npm/remixicon@3.4.0/fonts/remixicon.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Fragment+Mono&family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+        <title>beats</title>
 </head>
 <body>
 
@@ -24,8 +23,9 @@
             <div class="btn">
                 <i class='bx bx-play play' onclick="playBeat({{$key}})"></i>
                 <i class='bx bx-pause pause' onclick="pauseBeat({{$key}})" style="display: none"></i>
-                <i class='bx bx-link-alt link' ></i>
+                <a href="https://www.youtube.com/watch?v=zIL-kiHiQUU"><i class='bx bx-link link'></i></a>
             </div>
+            <p class="beat-tags">{{$beat->tags}}</p>
         </div>
     </div>
         @include('blocks.music-bar')
@@ -34,17 +34,19 @@
 
 <script src="{{asset('js/jquery-v3.7.0.min.js')}}"></script>
 <script>
+
     audio = $('.audio')
     play = $('.play')
     pause = $('.pause')
     bar= $('.bar')
+    black = $('.black')
 
     function playBeat(id){
-        audio.trigger('pause')
         audio.eq(id).trigger('play')
         play.eq(id).hide()
         pause.eq(id).show()
         bar.eq(id).css("display", "flex").show()
+        black.eq(id).css("display", "flex").show()
     }
 
     function pauseBeat(id){
--- resources/views/blocks/music-bar.blade.php
+++ resources/views/blocks/music-bar.blade.php
@@ -3,16 +3,19 @@
 <div class="bar">
     <div class="bar-start">
         <div class="logo-bar" style="background: url({{asset($beat->cover)}}); background-size: cover"></div>
-        <p class="title-bar">{{$beat->name}}</p>
+        <div class="text-bar">
+            <p class="title-bar">{{$beat->name}}</p>
+            <p class="title-bar" style="font-size: .8rem">{{$beat->tags}}</p>
+        </div>
     </div>
     <div class="bar-middle">
         <audio controls class="audio">
             <source src="{{asset($beat->audio)}}" type="audio/mp3">
         </audio>
     </div>
-    <div class="bar-end">
-        <i class='bx bx-play play-bar' onclick="playBeat()"></i>
-        <i class='bx bx-pause pause-bar' onclick="pauseBeat()"></i>
-    </div>
+{{--    <div class="bar-end">--}}
+{{--        <i class='bx bx-play play' style="font-size: 3rem; display: none" onclick="playBeat({{$key}})"></i>--}}
+{{--        <i class='bx bx-pause pause'  style="font-size: 3rem" onclick="pauseBeat({{$key}})"></i>--}}
+{{--    </div>--}}
 </div>
 
